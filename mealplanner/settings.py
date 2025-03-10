558b581d2b6401c7c7b943bf8122a18b6c56841b
jignasa-dcosta <jignasadcosta@gmail.com>
2025-03-10 21:21:05 +0530

    Update CSRF trusted origins for production deployment

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['railway.app', 'festiveeats-meal-planner-production.up.railway.app', '127.0.0.1', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://hospitalmgm-232d.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://festiveeats-meal-planner-production.up.railway.app']
 
 # Application definition
 
