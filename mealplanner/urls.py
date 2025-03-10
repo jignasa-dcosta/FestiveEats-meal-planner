05c0cf055f344df4903888de4092848bb08ee7ce
jignasa-dcosta <jignasadcosta@gmail.com>
2025-03-10 21:35:01 +0530

    Fix static file serving configuration in DEBUG mode

--- mealplanner/urls.py
+++ mealplanner/urls.py
@@ -38,8 +38,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
