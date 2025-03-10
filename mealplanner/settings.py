4949f3129d8e671cf94dbfa8c1438a200072199f
jignasa-dcosta <jignasadcosta@gmail.com>
2025-03-10 20:25:30 +0530

    Update allowed hosts in settings and modify requirements

--- mealplanner/settings.py
+++ mealplanner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e9tu@dwx$o8j2p&(69^#nm@0&i=!fd-@d&nur^sll^$+n27l2$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['railway.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
